ef420dbe56816115d018da9dd6ca772119779801
Yaroslav_Kratt <wtf0100@gmail.com>
2019-06-09 01:28:51 +0300

    kek bot

--- src/main/java/ua/kpi/pti/diploma/telegram_bot/StatisticSenderBot.java
+++ src/main/java/ua/kpi/pti/diploma/telegram_bot/StatisticSenderBot.java
@@ -15,17 +15,92 @@ import ua.kpi.pti.diploma.tables.*;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 //this is totaly shit? but works and
 public class StatisticSenderBot extends TelegramLongPollingBot {
     private long chatId;
+    Map<Long,Integer> map = new HashMap<>();
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Got message");
         chatId = update.getMessage().getChatId();
-        SendMessage message = new SendMessage().setChatId(chatId).setText("Lets go? /go");
-        try {
+        SendMessage message;
+        if(map.get(chatId)==0) {
+            message = new SendMessage().setChatId(chatId).setText("Ну привет, чё хотел?");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(map.get(chatId)==1) {
+            message = new SendMessage().setChatId(chatId).setText("И чё?");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(map.get(chatId)==2) {
+            message = new SendMessage().setChatId(chatId).setText("Начинаешь надоедать");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        if(map.get(chatId)==3) {
+            message = new SendMessage().setChatId(chatId).setText("Уходи, дверь закрой");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(map.get(chatId)==4) {
+            message = new SendMessage().setChatId(chatId).setText("Ну достал ужеб э!");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(map.get(chatId)==5) {
+            message = new SendMessage().setChatId(chatId).setText("у блэт, настойчивый, ну-ну");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        if(map.get(chatId)==5||map.get(chatId)==6||map.get(chatId)==7||map.get(chatId)==8||map.get(chatId)==9||map.get(chatId)==10||map.get(chatId)==11||map.get(chatId)==12) {
+            message = new SendMessage().setChatId(chatId).setText("Я могу так хоть весь день, а ты? Чё написал диплом? А? Больно да?  получай!!!");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        if (map.get(chatId)==13) {
+            message = new SendMessage().setChatId(chatId).setText("Ты победил, но но стоило оно того? Ты потратил время, которое больше не вернуть. ты мог сделать дела, которые уже несделаешь, но ты потратил своё драгоценное время на бездушную машиную! АЗАЗАЗ ЛАЛКА!");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        map.putIfAbsent(chatId,1);
+        map.put(chatId,map.get(chatId)+1);
+       // SendMessage message = new SendMessage().setChatId(chatId).setText("Lets go? /go");
+    /*    try {
             execute(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
@@ -35,7 +110,7 @@ public class StatisticSenderBot extends TelegramLongPollingBot {
 
             AgafonovCriteriaFilter agafonovCriteriaFilter = new AgafonovCriteriaFilter();
             List<TableProvider> tableProvidersPool = new ArrayList<>();
-            Type type = Type.AFFINE_ON_ENTER;
+            Type type = Type.AFFINE_ON_EXIT;
             message = new SendMessage().setChatId(chatId).setText("WORKING!!!!");
             try {
                 execute(message);
@@ -59,17 +134,17 @@ public class StatisticSenderBot extends TelegramLongPollingBot {
 
                 SendPhoto sendPhoto = new SendPhoto();
                 sendPhoto.setChatId(chatId)
-                        .setPhoto(new File (((BarChartForTables) chart).getPath()));
+                        .setPhoto(new File (((BarChartForTables) chart).getPath()));*/
 
-                try {
+               /* try {
                     execute(sendPhoto);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
 
-            });
+            });*/
 
-        }
+
     }
 
     @Override
